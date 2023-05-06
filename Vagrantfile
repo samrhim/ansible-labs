d76a1463cb7fbdbcc08d667d63ac22f87a855493
samrhim <rhimi1111@gmail.com>
2023-05-06 20:00:37 +0100

    Another modifications

--- Vagrantfile
+++ Vagrantfile
@@ -34,7 +34,7 @@ Vagrant.configure("2") do |config|
 	    node.vm.provision "shell", path: "managed.sh"
       node.vm.provider "virtualbox" do |vb|
         vb.name = "node0#{i}"
-        vb.memory = "2048"
+        vb.memory = "1024"
       end                
     end
   end
--- control.sh
+++ control.sh
@@ -8,7 +8,7 @@ sed -i 's|#baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g' /
 dnf update -y
 dnf install -y epel-release wget
 dnf makecache --refresh
-dnf install -y python39 python39-pip git bind-utils vim bash-completion libX11
+dnf install -y python39 python39-pip ansible git bind-utils vim bash-completion libX11
 wget http://mirror.centos.org/centos/8-stream/AppStream/x86_64/os/Packages/sshpass-1.09-4.el8.x86_64.rpm
 rpm -i sshpass-1.09-4.el8.x86_64.rpm
 
@@ -18,13 +18,18 @@ systemctl restart sshd
 export PATH="/usr/local/bin:$PATH"
 source  ~/.bash_profile
 
-dnf install -y ansible
-
 PASS=$(echo "control" | openssl passwd -1 -stdin)
-useradd -p "$PASS" master
-cat <<EOF > /etc/sudoers.d/master
-master	ALL=NOPASSWD: ALL
+useradd -p "$PASS" ansible
+cat <<EOF > /etc/sudoers.d/ansible
+ansible 	ALL=NOPASSWD: ALL
 EOF
+
+# alternatives --set python /usr/bin/python39
+# python39 -m pip install --upgrade pip
+python3 -m pip install --user wheel
+python3 -m pip install --user ansible==3.6.8
+ansible --version
+
 
 cat <<EOF > /etc/hosts
 192.168.30.200 control.clevory.local
@@ -35,6 +40,6 @@ EOF
 su - ansible -c "ssh-keygen -b 2048 -t rsa -f /home/ansible/.ssh/id_rsa -q -P \"\""
 su - ansible -c "ssh-keyscan  node01.clevory.local 2>/dev/null >> home/ansible/.ssh/known_hosts"
 su - ansible -c "ssh-keyscan  node02.clevory.local 2>/dev/null >> home/ansible/.ssh/known_hosts"
-su - ansible -c "echo 'ansible' |sshpass ssh-copy-id -f -i /home/ansible/.ssh/id_rsa.pub -o StrictHostKeyChecking=no ansible@ansible1.clevory.local"
-su - ansible -c "echo 'ansible' |sshpass ssh-copy-id -f -i /home/ansible/.ssh/id_rsa.pub -o StrictHostKeyChecking=no ansible@ansible2.clevory.local"
-su - ansible -c "git clone https://github.com/samrhim/rhce8-live.git"
+su - ansible -c "echo 'ansible' |sshpass ssh-copy-id -f -i /home/ansible/.ssh/id_rsa.pub -o StrictHostKeyChecking=no ansible@node01.clevory.local"
+su - ansible -c "echo 'ansible' |sshpass ssh-copy-id -f -i /home/ansible/.ssh/id_rsa.pub -o StrictHostKeyChecking=no ansible@node02.clevory.local"
+# su - ansible -c "git clone https://github.com/samrhim/rhce8-live.git"
